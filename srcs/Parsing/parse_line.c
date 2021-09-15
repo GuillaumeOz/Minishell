97fe17e99ff609181ee4be66bba8bcb672d481dd
chdespon <charles.despont@gmail.com>
2021-09-15 11:52:55 +0200

    🔀Merge main in to branch builtin

---
+++ srcs/Parsing/parse_line.c
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_line.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/29 12:12:52 by chdespon          #+#    #+#             */
+/*   Updated: 2021/09/13 12:08:55 by chdespon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	parse_splited_line(char **splited_line, char **env, int pipe)
+{
+	char	*cmd;
+	if (splited_line[0] == NULL)
+		return ;
+	else if (ft_strcmp(splited_line[0], "echo") == 0)
+	{
+		if (splited_line[1] != NULL && ft_strcmp(splited_line[1], "-n") == 0)
+			print_echo(splited_line, true);
+		else
+			print_echo(splited_line, false);
+	}
+	else if (ft_strcmp(splited_line[0], "cd") == 0)
+	{
+		if (splited_line[2] != NULL)
+		{
+			ft_putstr("cd: trop d'arguments\n");
+			return ;
+		}
+		else if (chdir(splited_line[1]) != 0)
+			printf("cd: %s: Aucun fichier ou dossier de ce type\n",
+				splited_line[1]);
+		change_pwd(&env, splited_line[1]);
+	}
+	else if (ft_strcmp(splited_line[0], "env") == 0
+		&& splited_line[1] == NULL)
+		print_env(env);
+	else if (ft_strcmp(splited_line[0], "pwd") == 0
+		&& splited_line[1] == NULL)
+		print_pwd();
+	else if (ft_strcmp(splited_line[0], "unset") == 0
+		&& splited_line[1] != NULL)
+		unset_env(splited_line[1], &env);
+	else if (ft_strcmp(splited_line[0], "export") == 0
+		&& splited_line[1] != NULL)
+		set_env(splited_line[1] , NULL, &env);
+	else
+	{
+		cmd = find_cmd(env, splited_line[0]);
+		if (pipe == 1)
+		{
+			if (execve(cmd, splited_line, env) == -1)
+				exit(EXIT_FAILURE);
+		}
+		else
+			launch_fork(splited_line, env, cmd);
+		free(cmd);
+		cmd = NULL;
+	}
+}
+
+void	pipe_fork(char **env, char *line)
+{
+	int		pipefd[2];
+	pid_t	cpid;
+	char	**splited_line;
+
+	splited_line = ft_split(line, '|');
+	// (void)env;
+	// if (ft_tab_len((void **)line) != 2)
+	// {
+	// 	fprintf(stderr, "Usage: %s <string>\n", line[0]);
+	// 	exit(EXIT_FAILURE);
+	// }
+	if (pipe(pipefd) == -1)
+		exit(EXIT_FAILURE);
+	cpid = fork();
+	if (cpid == -1)
+		exit(EXIT_FAILURE);
+	if (cpid == 0)
+	{	/* Child reads from pipe */
+		close(pipefd[1]);		/* Close unused write end */
+		dup2(pipefd[0], 0);
+		close(pipefd[0]);
+		parse_line(splited_line[1], env, 1);
+	}
+	else
+	{	/* Parent writes first command to pipe */
+		close(pipefd[0]);			/* Close unused read end */
+		dup2(pipefd[1], 1);
+		close(pipefd[1]);			/* Reader will see EOF */
+		parse_line(splited_line[0], env, 1);			/* Wait for child */
+	}
+}
+
+void	fork_minishell(char **env, char *line)
+{
+	pid_t	pid;
+	int		status;
+
+	status = 0;
+	pid = fork();
+	if (pid < 0)
+		exit(EXIT_FAILURE);
+	else if (pid == 0)
+		pipe_fork(env, line);
+	else
+	{
+		waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			waitpid(pid, &status, WUNTRACED);
+	}
+}
+
+void	parse_line(char *line, char **env, int pipe)
+{
+	char	**splited_line;
+	int		i;
+
+	if (line != NULL && ft_strcmp(line, "exit") == 0)
+	{
+		ft_free_tab((void **)env);
+		free(line);
+		ft_putstr("exit\n");
+		exit(EXIT_SUCCESS);
+	}
+	splited_line = ft_split(line, ' ');
+	i = 0;
+	while (splited_line[i] != NULL)
+	{
+		if (ft_strcmp(splited_line[i], "|") == 0 && splited_line[i + 1] != NULL)
+		{
+			fork_minishell(env, line);
+			ft_free_tab((void **)splited_line);
+			return ;
+		}
+		i++;
+	}
+	parse_splited_line(splited_line, env, pipe);
+	ft_free_tab((void **)splited_line);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:05:03 by chdespon          #+#    #+#             */
-/*   Updated: 2021/09/10 17:30:40 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/09/14 17:51:00 by chdespon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,18 @@ char	**alloc_env(char **env)
 	return (dest);
 }
 
+void	quit(int i, char **env)
+{
+	ft_free_tab((void **)env);
+	ft_putstr("exit\n");
+	rl_clear_history();
+	exit(i);
+}
+
 void	prompt(char **env)
 {
 	char	*line;
 
-	(void)env;
 	while (1)
 	{
 		line = readline(_BLUE"("_RED"Minishell" _BLUE") "_GREEN"➜"_WHITE" ");
@@ -39,7 +46,7 @@ void	prompt(char **env)
 			if (line != NULL && ft_strcmp(line, "\n") > 0
 				&& ft_str_is_only_whitespaces(line) == 0)
 			{
-				// parse_line(line, env, 0);
+				parse_line(line, env, 0);
 				add_history(line);
 			}
 			if (line == NULL)
@@ -48,6 +55,7 @@ void	prompt(char **env)
 		}
 	}
 	free(line);
+	quit(val_ret, env);
 }
 
 void	handle_sigint(int sig)
@@ -58,6 +66,7 @@ void	handle_sigint(int sig)
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
+		val_ret = 130;
 	}
 }
 
@@ -66,13 +75,11 @@ int	main(int ac, char **av, char **env)
 	char	**new_env;
 
 	(void)av;
-	if (ac != 1)
-		return (0);
+	if (ac != 1 && av[1] == NULL)
+		return (EXIT_FAILURE);
 	new_env = alloc_env(env);
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, handle_sigint);
 	prompt(new_env);
-	ft_free_tab((void **)new_env);
-	ft_putstr("exit\n");
-	return (0);
+	return (EXIT_SUCCESS);
 }

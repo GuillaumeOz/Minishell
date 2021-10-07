2e5a7c4cb971f36497644eb153e5fb5f6e94e2b1
Guillaume OZSERTTAS <gozsertt@e1r13p7.42.fr>
2021-10-07 17:38:26 +0200

    🗑️ - Minor cleaning

--- includes/minishell_tokenizer.h
+++ includes/minishell_tokenizer.h
@@ -6,14 +6,12 @@
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 15:05:05 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/09/18 18:38:07 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/10/07 17:38:10 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_TOKENIZER_H
 # define MINISHELL_TOKENIZER_H
-
-void print_token(t_lexer *lexer);//suppr this
 
 t_lexer	*args_token(t_lexer *lexer, char *line, int *i);
 t_lexer	*greater_token(t_lexer *lexer, char *line, int *i);

c34611ff8b778250bffcfb61ff711f3b57ebdbe9
chdespon <charles.despont@gmail.com>
2021-10-20 16:36:47 +0200

    🐛 - fix some minor bug and builtins

--- srcs/prompt/minishell_prompt.c
+++ srcs/prompt/minishell_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_prompt.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 16:02:51 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/10/19 15:26:23 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/10/20 16:22:02 by chdespon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	prompt(char ***env)
 
 	while (1)
 	{
-		line = readline(_BLUE"("_RED"Minishell" _BLUE") "_GREEN"➜"_WHITE" ");
+		line = readline(_GREEN"➜ "_BLUE"("_RED"Minishell" _BLUE") "
+			_YELLOW"✗"_WHITE" ");
 		lexer = tokenizer(line, env);
 		if (line != NULL && ft_strcmp(line, "\n") > 0
 			&& ft_str_is_only_whitespaces(line) == false)

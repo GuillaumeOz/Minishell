91f3c0f8a216acc61852b47f0e9a7ce3d6954ec7
Guillaume Ozserttas <guillaume.ozserttas@gmail.com>
2021-10-24 22:06:04 +0200

    🐛 - Fix bug, enter doesn't reset the global return to 0

--- srcs/prompt/minishell_prompt.c
+++ srcs/prompt/minishell_prompt.c
@@ -6,7 +6,7 @@
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 16:02:51 by gozsertt          #+#    #+#             */
-/*   Updated: 2021/10/24 15:41:26 by gozsertt         ###   ########.fr       */
+/*   Updated: 2021/10/24 22:04:10 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	prompt(char ***env)
 		}
 		if (line == NULL)
 			break ;
+		if (ft_strlen(line) == 0 && g_exit_code != 0)
+			g_exit_code = 0;
 		free(line);
 		free_lexer(lexer);
 	}
